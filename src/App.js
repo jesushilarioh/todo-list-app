deac937e4ede41e82a3010a277c131725745d320
Jesus Hilario Hernandez <jesushilariohernandez@gmail.com>
2019-08-20 22:57:39 -0500

    added AddTodo.js, layout, pages, installed react-router-dom and axios, modified App.js, TodoItem.js, Todos.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import About from './components/pages/About'
+import Header from './components/layout/Header'
 import Todos from './components/Todos'
+import AddTodo from './components/AddTodo'
+import uuid from 'uuid'
 
 import './App.css';
 
@@ -7,29 +12,90 @@ class App extends Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuid.v4(),
         title: 'Take out the trash',
-        completed: false
+        completed: true
       },
       {
-        id: 2,
+        id: uuid.v4(),
         title: 'Dinner with wife',
         completed: false
       },
       {
-        id: 3,
+        id: uuid.v4(),
         title: 'Meeting with boss',
         completed: false
       }
     ]
   }
 
+  // Toggle Complete
+  markComplete = (id) => {
+    // console.log('id', id)
+    this.setState(
+      {
+        todos: this.state.todos.map(todo => {
+          if (todo.id === id) {
+            todo.completed = !todo.completed
+          }
+          return todo
+        })
+      }
+    ) 
+  }
+  // Add Todo
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuid.v4(),
+      title,
+      completed: false
+    }
+    this.setState(
+      {
+        todos: [...this.state.todos, newTodo]
+      }
+    )
+  }
+  // Delete Todo
+  delTodo = (id) => {
+    // console.log('id', id)
+    this.setState(
+      {
+        todos: [...this.state.todos.filter(
+          todo => todo.id !== id
+          )
+        ]
+      }
+    )
+  }
+
   render() {
     // console.log(this.state.todos)
     return (
-      <div className="App">
-        <Todos todos={ this.state.todos }/>
-      </div>
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route 
+              exact
+              path="/" 
+              render={ props => (
+                <React.Fragment>
+                  <AddTodo addTodo={ this.addTodo } />
+                  <Todos 
+                    todos={ this.state.todos } 
+                    markComplete={ this.markComplete } 
+                    delTodo={ this.delTodo }
+                  />
+                </React.Fragment>
+              ) } 
+            />
+            <Route 
+              path="/about"
+              component={ About }/>
+          </div>
+        </div>
+      </Router>
     )
   }
 
